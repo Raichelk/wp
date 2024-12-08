09511d6551207ffcc32262240cba1f7077410ef1
Raichel Biju <raichelkundukulam@gmail.com>
2024-12-08 18:46:34 +0000

    Add new file

---
+++ wp-config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', '${DB_NAME}');
+define('DB_USER', '${DB_USER}');
+define('DB_PASSWORD', '${DB_PASSWORD}');
+define('DB_HOST', '${DB_HOST}');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
