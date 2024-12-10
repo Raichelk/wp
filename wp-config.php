f0240b0db8813ab66968d4e9902c25f878cf3c3f
Raichel Biju <raichelkundukulam@gmail.com>
2024-12-10 06:27:17 +0000

    Delete wp-config.php

--- wp-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_NAME', '${DB_NAME}');
-define('DB_USER', '${DB_USER}');
-define('DB_PASSWORD', '${DB_PASSWORD}');
-define('DB_HOST', '${DB_HOST}');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-
